6be749657c4280080315c93d0fa8d76a58dfec17
abdyevas <sabina.abdyeva@ufaz.az>
2021-06-10 15:24:47 +0400

    added comments

--- game.c
+++ game.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <time.h>
 #include "game.h"
+
 void display_board(Board board)
 {
     int i, j;
@@ -12,7 +13,9 @@ void display_board(Board board)
     {
         for(j = 0; j < COLS; j++)
         {
+            // state 1 is for alive cell, initialized as #
             if(board[i][j] == 1) printf("#");
+            // state 0 if for dead cell, initialized as -
             else printf("-");
         }
         printf("\n");
@@ -27,13 +30,15 @@ int count_neighbors(Board board, int x, int y)
     {
         for(j = -1; j < 2; j++)
         {
+            // find the positions of cell's neighbors
             row = (x + i + ROWS) % ROWS;
             col = (y + j + COLS) % COLS;
 
             neighbors += board[row][col]; 
         }
     }
-    neighbors -= board[row][col];
+    // subtract the position of cell 
+    neighbors -= board[x][y];
     return neighbors;
 }
 
@@ -45,10 +50,15 @@ void compute_new_board(Board board_t,Board board_t_1)
         for(j = 0; j < COLS; j++)
         {
             state = board_t[i][j];
+
+            // count neighbors for each cell
             n_neighbors = count_neighbors(board_t, i, j);
 
+            // cell becomes alive if it has exactly 3 neighbors
             if(state == 0 && n_neighbors == 3) board_t_1[i][j] = 1;
+            // cell dies if it has less than two or more than three neighbors
             else if(state == 1 && (n_neighbors < 2 || n_neighbors > 3)) board_t_1[i][j] = 0;
+            // otherwise cell stays at the same state
             else board_t_1[i][j] = board_t[i][j];
         }
     }
--- main.c
+++ main.c
@@ -7,8 +7,7 @@
 #include "game.h"
 
 int main() 
-{
-    srand(time(0));
+{    
     // create 2D board at time t
     Board board_t = 
     {
@@ -19,74 +18,31 @@ int main()
         {0,0,0,0,0,0},
     };
 
-    // create initially empty 2D board at time t+1
+    // create 2D board at time t+1
     int board_t_1[ROWS][COLS];
+    // copy characters of board at time t to the board at time t+1
     memcpy(board_t_1, board_t, sizeof(board_t));
 
+    // create pointers to the boards 
     Board *ptr_board_t = &board_t;
     Board *ptr_board_t_1 = &board_t_1;
 
     while(true)
     {
+        // clear the console after each iteraction
 		printf("\033[H\033[J");
         display_board(*ptr_board_t);
 
         compute_new_board(*ptr_board_t, *ptr_board_t_1);
 
+        // create temporary board to repeat iteraction with the board at time t+1
+        // i.e board at time t+1 becomes board at time t to continue iteractions
         Board *tmp_board = ptr_board_t;
         ptr_board_t = ptr_board_t_1;
         ptr_board_t_1 = tmp_board;
 
+        // wait before each iteraction
         usleep(250000);
     }    
     return 0;
-}
-
-void display_board(Board board)
-{
-    int i, j;
-    for(i = 0; i < ROWS; i++)
-    {
-        for(j = 0; j < COLS; j++)
-        {
-            if(board[i][j] == 1) printf("#");
-            else printf("-");
-        }
-        printf("\n");
-    }
-}
-
-int count_neighbors(Board board, int x, int y)
-{
-    int neighbors = 0, i, j, row, col;
-
-    for(i = -1; i < 2; i++)
-    {
-        for(j = -1; j < 2; j++)
-        {
-            row = (x + i + ROWS) % ROWS;
-            col = (y + j + COLS) % COLS;
-
-            neighbors += board[row][col]; 
-        }
-    }
-    neighbors -= board[row][col];
-    return neighbors;
-}
-
-void compute_new_board(Board board_t,Board board_t_1)
-{
-    int i, j, state, n_neighbors;
-    for(i = 0; i < ROWS; i++)
-    {
-        for(j = 0; j < COLS; j++)
-        {
-            state = board_t[i][j];
-            n_neighbors = count_neighbors(board_t, i, j);
-
-            if(state == 0 && n_neighbors == 3) board_t_1[i][j] = 1;
-            else if(state == 1 && (n_neighbors < 2 || n_neighbors > 3)) board_t_1[i][j] = 0;
-            else board_t_1[i][j] = board_t[i][j];
-        }
-    }
 }
