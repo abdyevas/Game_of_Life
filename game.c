89543007242a623f5d0d738457c04ba26debf5eb
abdyevas <sabina.abdyeva@ufaz.az>
2021-06-11 00:26:50 +0400

    comments added

--- game.c
+++ game.c
@@ -13,13 +13,8 @@ void display_board(Board board)
     {
         for(j = 0; j < COLS; j++)
         {
-            // state 1 is for alive cell, initialized as #
-            //if(board[i][j] == 1) printf("#");
-            //if(board[i][j] == 1) printf("\033[104m");
-                
-            // state 0 if for dead cell, initialized as -
-            //else printf("-");
-            //else printf("\033[41m");
+            // state 1 is for alive cell, colored at magenta 
+            // state 0 if for dead cell, colorless
             int c = (board[i][j] == 1) ? 104 : 0;
 			printf("\033[%dm  ",c);
 			
